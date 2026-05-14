b8831674105ab5489b90027f13140a6221b0eb6c
d4rp4t <d4rp4t@tutanota.com>
2026-05-14 17:10:57 +0200

    hash to scalar

---
+++ DotNut.BLS12-381/HashToCurve/HashToCurveMapper.Scalar.cs
@@ -0,0 +1,18 @@
+namespace DotNut.BLS12_381.HashToCurve;
+
+public static partial class HashToCurveMapper
+{
+    internal static Scalar ScalarFromOkm(ReadOnlySpan<byte> okm)
+    {
+        if (okm.Length != 48)
+        {
+            throw new ArgumentException("OKM must be 48 bytes.", nameof(okm));
+        }
+
+        Span<byte> bs = stackalloc byte[64];
+        bs.Clear();
+        okm.CopyTo(bs[16..]);
+        bs.Reverse();
+        return Scalar.FromBytesWide(bs);
+    }
+}
