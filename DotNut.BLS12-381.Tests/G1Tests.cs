54b0b697ff0d72fbda8c9e00130c13ffefd6e6c7
d4rp4t <d4rp4t@tutanota.com>
2026-05-17 23:18:06 +0200

    rm redundant tests

--- DotNut.BLS12-381.Tests/G1Tests.cs
+++ DotNut.BLS12-381.Tests/G1Tests.cs
@@ -11,115 +11,6 @@ public sealed class G1Tests
         System.Globalization.NumberStyles.AllowHexSpecifier
     );
 
-    #region BasicProperties
-
-    [Fact]
-    public void Generator_ShouldBeOnCurve()
-    {
-        Assert.True(G1Affine.Generator.IsOnCurve());
-    }
-
-    [Fact]
-    public void Infinity_Roundtrip_ProjectiveAffine()
-    {
-        var inf = G1Projective.Infinity.ToAffine();
-        Assert.True(inf.IsInfinity);
-    }
-
-    [Fact]
-    public void AffineProjective_Roundtrip_ShouldPreservePoint()
-    {
-        var g = G1Affine.Generator;
-        var back = g.ToProjective().ToAffine();
-        Assert.True(AffineEqual(g, back));
-    }
-
-    [Fact]
-    public void Infinity_ShouldBehaveAsNeutralElement()
-    {
-        var g = G1Affine.Generator.ToProjective();
-        Assert.True(AffineEqual(G1Projective.Add(g, G1Projective.Infinity).ToAffine(), g.ToAffine()));
-        Assert.True(AffineEqual(G1Projective.Add(G1Projective.Infinity, g).ToAffine(), g.ToAffine()));
-    }
-
-    #endregion
-
-    #region Arithmetic
-
-    [Fact]
-    public void Double_ShouldMatch_AddSelf()
-    {
-        var g = G1Affine.Generator.ToProjective();
-        var d = G1Projective.Double(g).ToAffine();
-        var a = G1Projective.Add(g, g).ToAffine();
-        Assert.True(AffineEqual(d, a));
-    }
-
-    [Fact]
-    public void Results_ShouldStayOnCurve()
-    {
-        var g = G1Affine.Generator.ToProjective();
-        var p = g;
-        for (var i = 0; i < 20; i++)
-        {
-            p = G1Projective.Add(p, g);
-            Assert.True(p.IsOnCurve());
-        }
-    }
-
-    [Fact]
-    public void Jacobian_Add_And_Double_ShouldMatch_AffineReference()
-    {
-        var random = new Random(9001);
-        var g = G1Affine.Generator;
-
-        for (var i = 0; i < 40; i++)
-        {
-            var k1 = random.Next(0, 1 << 16);
-            var k2 = random.Next(0, 1 << 16);
-
-            var pJac = G1Projective.ScalarMultiply(g.ToProjective(), Scalar.FromBigInteger(new BigInteger(k1)));
-            var qJac = G1Projective.ScalarMultiply(g.ToProjective(), Scalar.FromBigInteger(new BigInteger(k2)));
-            var jacAdd = G1Projective.Add(pJac, qJac).ToAffine();
-            var jacDbl = G1Projective.Double(pJac).ToAffine();
-
-            var pAff = AffineScalarMultiply(g, k1);
-            var qAff = AffineScalarMultiply(g, k2);
-            var affAdd = AffineAdd(pAff, qAff);
-            var affDbl = AffineDouble(pAff);
-
-            Assert.True(AffineEqual(jacAdd, affAdd));
-            Assert.True(AffineEqual(jacDbl, affDbl));
-        }
-    }
-
-    #endregion
-
-    #region ScalarMultiply
-
-    [Fact]
-    public void ScalarMul_SmallValues_ShouldBeConsistent()
-    {
-        var g = G1Affine.Generator.ToProjective();
-        var one = G1Projective.ScalarMultiply(g, Scalar.One).ToAffine();
-        var two = G1Projective.ScalarMultiply(g, Scalar.FromBigInteger(2)).ToAffine();
-        var three = G1Projective.ScalarMultiply(g, Scalar.FromBigInteger(3)).ToAffine();
-
-        Assert.True(AffineEqual(one, G1Affine.Generator));
-        Assert.True(AffineEqual(two, G1Projective.Double(g).ToAffine()));
-        Assert.True(AffineEqual(three, G1Projective.Add(g, G1Projective.Double(g)).ToAffine()));
-    }
-
-    [Fact]
-    public void ScalarMul_Zero_ShouldReturn_Infinity()
-    {
-        var g = G1Affine.Generator.ToProjective();
-        var zero = G1Projective.ScalarMultiply(g, Scalar.Zero).ToAffine();
-        Assert.True(zero.IsInfinity);
-    }
-
-    #endregion
-
     #region ExternalVectors
 
     [Fact]
@@ -152,7 +43,6 @@ public sealed class G1Tests
     [Fact]
     public void SubgroupCheck_RTimesGenerator_ShouldBeInfinity()
     {
-        // Source: https://www.rfc-editor.org/rfc/rfc9380.html#appendix-J.4
         var g = G1Affine.Generator.ToProjective();
         var result = G1Projective.ScalarMultiply(g, Scalar.FromBigInteger(GroupOrderR)).ToAffine();
         Assert.True(result.IsInfinity);
@@ -178,46 +68,6 @@ public sealed class G1Tests
         var x = Fp.FromBytesBigEndian(Convert.FromHexString(xPadded[32..]));
         var y = Fp.FromBytesBigEndian(Convert.FromHexString(yPadded[32..]));
         return new G1Affine(x, y);
-    }
-
-    private static G1Affine AffineAdd(G1Affine a, G1Affine b)
-    {
-        if (a.IsInfinity) return b;
-        if (b.IsInfinity) return a;
-
-        if (Fp.Equal(a.X, b.X))
-            return Fp.Equal(a.Y, b.Y) ? AffineDouble(a) : G1Affine.Infinity;
-
-        var lambda = Fp.Multiply(Fp.Subtract(b.Y, a.Y), Fp.Invert(Fp.Subtract(b.X, a.X)));
-        var x3 = Fp.Subtract(Fp.Subtract(Fp.Square(lambda), a.X), b.X);
-        var y3 = Fp.Subtract(Fp.Multiply(lambda, Fp.Subtract(a.X, x3)), a.Y);
-        return new G1Affine(x3, y3);
-    }
-
-    private static G1Affine AffineDouble(G1Affine a)
-    {
-        if (a.IsInfinity || Fp.Equal(a.Y, Fp.Zero)) return G1Affine.Infinity;
-        var x2 = Fp.Square(a.X);
-        var threeX2 = Fp.Add(x2, Fp.Add(x2, x2));
-        var twoY = Fp.Add(a.Y, a.Y);
-        var lambda = Fp.Multiply(threeX2, Fp.Invert(twoY));
-        var x3 = Fp.Subtract(Fp.Square(lambda), Fp.Add(a.X, a.X));
-        var y3 = Fp.Subtract(Fp.Multiply(lambda, Fp.Subtract(a.X, x3)), a.Y);
-        return new G1Affine(x3, y3);
-    }
-
-    private static G1Affine AffineScalarMultiply(G1Affine p, int k)
-    {
-        var result = G1Affine.Infinity;
-        var cur = p;
-        var e = k;
-        while (e > 0)
-        {
-            if ((e & 1) != 0) result = AffineAdd(result, cur);
-            cur = AffineDouble(cur);
-            e >>= 1;
-        }
-        return result;
     }
 
     #endregion
